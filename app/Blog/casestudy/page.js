00d8a178a9217e3fbd254f9b28a820c42937f735
Darshil Ajudia <darshilajudia.7@gmail.com>
2026-06-30 22:14:58 +0530

    solve redirection error

--- app/Blog/casestudy/page.js
+++ app/Blog/casestudy/page.js
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import React, { useState } from "react";
-import "../installation/style.css";
+import "./style.css";
 
 export default function CaseStudies() {
     // Software Platforms & Solutions filters
